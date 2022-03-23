4b8268a39d31ca006e65129add652b48a5442526
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-24 02:34:04 +0600

    add button cart desing

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -7,6 +7,15 @@ const Product = ({ product }) => {
     return (
         <div className="product">
             <img src={img} alt="" />
+            <div className="product-info">
+                <p className="product-name">{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller : {seller} star</small></p>
+                <p><small>Ratings : {ratings}</small></p>
+            </div>
+            <button className="btn-cart">
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
